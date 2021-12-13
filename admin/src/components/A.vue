b8da85ddd17428daa3c34ec82178b32523e8eb8d
xuexingwei <xuexingwei@fenxiangbuy.com>
2021-12-13 19:19:54 +0800

    init project

---
+++ admin/src/components/A.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>测试</h1>
+</template>
+
+
+<script lang="ts" setup>
+    
+
+</script>
---
+++ admin/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import usePluginImport from "vite-plugin-importer";
+import ViteComponents, {
+  AntDesignVueResolver,
+} from "vite-plugin-components/dist";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 支持 less 样式
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  plugins: [
+    // vite 支持 vue 单页面
+    vue(),
+    usePluginImport({
+      libraryName: "ant-design-vue",
+      libraryDirectory: "es",
+      style: true,
+    }),
+    // ant-design-vue 按需导入
+    ViteComponents({
+      customComponentResolvers: [AntDesignVueResolver()],
+    }),
+  ],
+});
