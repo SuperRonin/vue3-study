93d424e6a5209673e66d592dc0317cec2507951e
xuexingwei <xuexingwei@bus365.com>
2021-12-14 23:30:38 +0800

    修改依赖dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import usePluginImport from 'vite-plugin-importer';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import usePluginImport from "vite-plugin-importer";
 import ViteComponents, {
   AntDesignVueResolver
-} from 'vite-plugin-components/dist';
+} from "vite-plugin-components/dist";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,8 +19,8 @@ export default defineConfig({
     // vite 支持 vue 单页面
     vue(),
     usePluginImport({
-      libraryName: 'ant-design-vue',
-      libraryDirectory: 'es',
+      libraryName: "ant-design-vue",
+      libraryDirectory: "es",
       style: true
     }),
     // ant-design-vue 按需导入
